0cbda0604469d4dfab997eed7ef43ce75ea85c8a
Joe Bourne <Joe-Bourne@users.noreply.github.com>
2025-04-19 09:10:05 +0100

    Update DockerBuildAndRun.cmd

--- DockerBuildAndRun.cmd
+++ DockerBuildAndRun.cmd
@@ -1,10 +1,17 @@
 @ECHO OFF
+SETLOCAL
 
+:: Get the folder where this script is located
+SET SCRIPT_DIR=%~dp0
+CD /D %SCRIPT_DIR%
 
-Echo docker Build...
+Echo Docker Build...
 docker build -t order-manager-app .
 
 Echo Docker Compose...
 docker-compose up
 
+ENDLOCAL
 
+
+pause
